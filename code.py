7e98d435bc4e8f9f518bededdcb717227ee59fd7
Elvis Tai <68021244+ElvisTai@users.noreply.github.com>
2020-08-26 23:38:24 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+code = '''
+8MqUhk9uNojVec8k
+oA83N07xZSHmbRT6
+eBvHaOstTKCe4JTw
+ESksccrpbyzh4wLp
+ORQsJ5msYeNm2Hbi
+hbaE5AF23JRkbDSx
+u5YftkgF0Do9GTvQ
+kCRY0bQ6KvoMjus6
+gJNZfZX5rAJORDEZ
+R6SfnJpy97fg4WsU
+'''
